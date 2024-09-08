c4188662650da28bff6344929e625938b7ee981c
treppenwit <henryjosephhanna@gmail.com>
2024-09-08 15:23:16 +0000

    k

---
+++ Assignment/KattisEcho/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    string word;
+    cin >> word >> endl;
+    cout << word << ' ' << word << ' ' << word << endl;
+
+
+
+
+    return 0;
+}
