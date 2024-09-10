0e677ca2f3c46e8bbf481582747b848d569a8303
treppenwit <henryjosephhanna@gmail.com>
2024-09-10 04:08:26 +0000

    commit

--- Assignment/KattisEcho/main.cpp
+++ Assignment/KattisEcho/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main ()
 {
     string word;
-    cin >> word >> endl;
+    cin >> word;
     cout << word << ' ' << word << ' ' << word << endl;
 
 
---
+++ Assignment/Project1/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main ()
+{
+    string units;
+
+
+
+
+
+    cout << "How would you like units displayed? (feet or meters)" << endl;
+    cin >> units;
+    if (units = "feet"){
+        cout << "You have entered feet" << endl;
+        if (units = "meters")
+        cout << "You have entered meters" << endl;
+    }
+
+    return 0;
+}
