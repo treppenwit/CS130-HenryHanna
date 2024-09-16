1319310b44ba2a4d1b9a7e61fd5389c8c869770f
treppenwit <henryjosephhanna@gmail.com>
2024-09-16 22:27:49 +0000

    h

--- Assignment/Sep11/main.cpp
+++ Assignment/Sep11/main.cpp
@@ -7,12 +7,18 @@ int addTwo(int num1, int num2)
     return num1+num2;
 }
 
+void printCard(void)
+{
+    string card = R"|======|\n|     |\n|     |\n|     |======|\n";
+    cout << card << endl;
+    return 0;
+}
 int main(void)
 {
     int a, b;
     cin >> a >> b;
     cout << addTwo(a, b) << endl;
-
+    cout << printCard << endl;
 
 
     return 0;
