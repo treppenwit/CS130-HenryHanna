b9cc0fcd22e0486bb5e36a14229837814eb0d30d
treppenwit <henryjosephhanna@gmail.com>
2024-10-02 16:50:46 +0000

    west

---
+++ finalproject/western.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// functions for the story, probably randomizers
+
+int main()
+{
+    string name, nameLast;
+
+    cout
+
+    cout << "Who is the nobody's name?   ";
+    cin >> name >> nameLast;
+    cout << name << ' ' << nameLast << endl;
+
+
+
+
+
+
+
+
+
+
+
+    cin.ignore(100, '\n');
+    cout << "You have completed ending #. (press enter to restart)\n";
+    cin.get();
+    cout << "Good bye... for now" << endl;
+    return 0;
+}
