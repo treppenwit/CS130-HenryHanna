3e5a9f00b57b737f36ed942cfe000fccc1519963
treppenwit <henryjosephhanna@gmail.com>
2024-10-03 20:59:03 +0000

    final

--- finalproject/western.cpp
+++ finalproject/western.cpp
@@ -1,19 +1,48 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
 
 using namespace std;
 
 // functions for the story, probably randomizers
+string centerText(int width, const string& str) {
+    int len = str.length();
+    if(width < len) { 
+        return str; 
+    }
+    int diff = width - len;
+    int pad1 = diff/2;
+    int pad2 = diff - pad1;
+    return string(pad1, ' ') + str + string(pad2, ' ');
+}
+ 
 
 int main()
 {
-    string name, nameLast;
+    string name, nameLast, choice1;
 
-    cout
-
+    cout << centerText(100, "The wise shall inherit glory") << endl;
+    cout << centerText(100, "But shame shall be the legacy of fools.") << endl;
+    cout << centerText(100, "Proverbs 3:25") << endl;
+    cout << "\n" << endl;
     cout << "Who is the nobody's name?   ";
     cin >> name >> nameLast;
-    cout << name << ' ' << nameLast << endl;
+    cout << "\n" << endl;
+    
+    cout << "You've awoken in a cloud of troublesome thought. You're mind is fuzzy and dull, you don't know where you are. It's dark and cramped." << endl;
+    cout << "For whatever reason you might be in here, you feel it had to do with the night prior to this objectionable morning." << endl; 
+    cout << "Do you get up? (type yes or no)" << endl;
+    cin >> choice1;
+
+    if (choice1 == "yes")
+    {
+        cout << "You move slightly, only to realize you're stuffed in a chimney. Why on Earth are you in a chimney?" << endl;
+    }
+    if (choice1 == "no")
+    {
+        
+    }
+    
 
 
 
